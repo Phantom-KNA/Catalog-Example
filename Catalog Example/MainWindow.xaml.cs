48f2073f0346a6c783481b2ffd25547a99b54006
Phantom_KNA <rgavilaneslara@gmail.com>
2022-04-15 09:43:16 +0300

    Agregar archivos de proyecto.

---
+++ Catalog Example/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using DM.MovieApi;
+using DM.MovieApi.ApiResponse;
+using DM.MovieApi.MovieDb.Movies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Catalog_Example
+{
+
+
+    //Thread _newTaks;
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Search();
+        }
+
+        private async Task Search()
+        {
+            string bearerToken = "your-bearer-token-from-TheMovieDb.org";
+
+            // RegisterSettings only needs to be called one time when your application starts-up.
+            MovieDbFactory.RegisterSettings(bearerToken);
+
+            var movieApi = MovieDbFactory.Create<IApiMovieRequest>().Value;
+
+            ApiSearchResponse<MovieInfo> response = await movieApi.SearchByTitleAsync("Star Trek");
+
+            foreach (MovieInfo info in response.Results)
+            {
+                Console.WriteLine($"{info.Title} ({info.ReleaseDate}): {info.Overview}");
+            }
+        }
+    }
+}
